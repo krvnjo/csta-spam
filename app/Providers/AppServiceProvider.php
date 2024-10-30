cc833e51061c7dd018bd6141acffecc14dd35eae
krvnjo <khj.quimora@gmail.com>
2024-10-31 01:03:39 +0800

    Modify can blade directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,35 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Blade::directive('can', function ($expression) {
-            return "<?php if(Auth::check() && Auth::user()->role && {$this->buildPermissionCheck($expression)}): ?>";
+        Blade::directive('can', function ($permission) {
+            return "<?php if (auth()->check() && auth()->user()->hasPermissionTo($permission)): ?>";
         });
 
         Blade::directive('endcan', function () {
-            return "<?php endif; ?>";
+            return '<?php endif; ?>';
         });
-    }
-
-    /**
-     * Build the permission check logic dynamically based on the expression.
-     *
-     * @param string $expression
-     * @return string
-     */
-    protected function buildPermissionCheck(string $expression): string
-    {
-        preg_match('/([^&|]+)(&&|\|\|)?([^&|]*)/', $expression, $matches);
-
-        $permission1 = trim($matches[1]);
-        $operator = isset($matches[2]) ? trim($matches[2]) : '||';
-        $permission2 = isset($matches[3]) ? trim($matches[3]) : '';
-
-        $permissionCheck = "Auth::user()->role->permissions->pluck('name')->contains({$permission1})";
-
-        if (!empty($permission2)) {
-            $permissionCheck .= " {$operator} Auth::user()->role->permissions->pluck('name')->contains({$permission2})";
-        }
-
-        return $permissionCheck;
     }
 }
