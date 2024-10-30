3179b4969b2475da69b861a834584e9ed51f4093
krvnjo <khj.quimora@gmail.com>
2024-10-31 00:25:03 +0800

    Modify middlewares

--- app/Http/Middleware/NoCache.php
+++ app/Http/Middleware/NoCache.php
@@ -19,7 +19,7 @@ class NoCache
     {
         $response = $next($request);
 
-        return $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0')
+        return $response->header('Cache-Control', 'no-store, no-cache, must-revalidate')
             ->header('Pragma', 'no-cache')
             ->header('Expires', '0');
     }
